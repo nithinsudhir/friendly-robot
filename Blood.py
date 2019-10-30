639c7e6a49e2e91d1735f07817a879416b2f8569
Nithin Sudhir <nithin.sudhir@gmail.com>
2019-10-30 17:10:27 +1100

    Start class files for Donor, Blood, and Request

---
+++ Blood.py
@@ -0,0 +1,4 @@
+class Blood:
+    def __init__(self,type,amount):
+        self.type = type
+        self.amount = amount
---
+++ Donor.py
@@ -0,0 +1,9 @@
+class Donor:
+    def __init__(self,first_name, last_name, age, email, contact_phone,blood_type, allergens):
+        self.first_name = first_name 
+        self.last_nam = last_nam 
+        self.age = age 
+        self.email = email 
+        self.contact_phone = contact_phone 
+        self.lood_type = lood_type 
+        self.allergens = allergens 
