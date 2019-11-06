877b1ffa31b83ebc1b5c13bd0ed95bb8e61699e9
Nithin Sudhir <nithinsudhir@gmail.com>
2019-11-07 10:33:51 +1100

    the biggest and most important commit

--- run.py
+++ run.py
@@ -16,7 +16,18 @@ def login(current_user):
         current_user = input("\nPlease enter your user type (A/H/D): ")
         user = login(current_user)
         return user
+logo = """
+  _____         __             _____        _    
+ |  __ \       / _|           |  __ \      | |   
+ | |  | | __ _| |_ _ __  _   _| |  | |_   _| | __
+ | |  | |/ _` |  _| '_ \| | | | |  | | | | | |/ /
+ | |__| | (_| | | | | | | |_| | |__| | |_| |   < 
+ |_____/ \__,_|_| |_| |_|\__, |_____/ \__,_|_|\_\\
+                          __/ |                  
+                         |___/                   
+"""
 
+print('\033[93m'+logo+'\033[0m')
 print("Welcome to the DafnyDuk Blood Managment System")
 
 print("\nThis system accepts three different types of users:\nA - Administrator\nH - Hospital\nD - Donor")
