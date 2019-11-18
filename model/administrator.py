b1ce064f5c94a834f95b65fac5c876d69f38a4c4
Shane Kadish <shanekadish@hotmail.com>
2019-11-18 15:27:20 +1100

    fixed permissions, add/remove donor/deposit id bug, added ui stuff

--- model/administrator.py
+++ model/administrator.py
@@ -1,18 +1,114 @@
 from model.user import User
+from misc.utility_functions import *
+import re
 
 class Administrator(User):
     def __init__(self, system):
         self.system = system
-        self.user_type = 'Administrator'
-
-    def add_deposit(self, donor_id, blood_type, expiry_date, amount):
-        self.system.add_deposit(donor_id, blood_type, expiry_date, amount)
-
-    def remove_deposit(self, deposit_id):
-        self.system.remove_deposit(deposit_id)
-
-    def add_donor(self, first_name, last_name, age, blood_type, email, allergens):
-        self.system.add_donor(self, first_name, last_name, age, blood_type, email, allergens)
-
-    def remove_donor(self, donor_id):
+        
+    def add_donor(self):
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        age = int(input("Enter age: "))
+        blood_type = type_to_int(input("Enter blood type (A|B|AB|O)[+-]: "))
+        email = input("Enter email: ")
+        allergens = input("Enter allergens (N/A if none): ")
+        self.system.add_donor(first_name, last_name, age, blood_type, email, allergens)
+        print("Donor added succesfully")
+    
+    def remove_donor(self):
+        donor_id = int(input('Enter donor ID: '))
         self.system.remove_donor(donor_id)
+        print('Donor removed successfully')
+
+
+    def count_deposits(self):
+        option = input('Enter (A|B|AB|O)[+-] to specify type [optional]: ')
+        if not option:
+            print(self.system.count_deposits())
+        elif re.match(r'^(A|B|AB|O)[+-]$', option):
+            blood_type = type_to_int(option)
+            print(self.system.count_deposits(blood_type))
+        else:
+            print('Unrecognised blood type, please try again.')
+            self.count_deposits()
+
+    def warn_scarce_blood_types(self):
+        blood_total_lower_limit = 500
+        scarce = self.system.get_scarce_blood_types(blood_total_lower_limit)
+        if (scarce != []):
+            print_scarce_blood(scarce)
+
+    def count_volume(self):
+        option = input('Enter (A|B|AB|O)[+-] to specify type [optional]: ')
+        if not option:
+            print(self.system.count_volume())
+        elif re.match(r'^(A|B|AB|O)[+-]$', option):
+            blood_type = type_to_int(option)
+            print(self.system.count_volume(blood_type))
+        else:
+            print('Unrecognised blood type, please try again.')
+            self.count_volume()
+    
+    def add_deposit(self):
+        donor_id = int(input('Enter donor ID: '))
+        blood_type = type_to_int(input('Enter blood type (A|B|AB|O)[+-]: '))
+        expiry_date = date_to_int(input('Enter expiry date (dd/mm/yyyy): '))
+        amount = int(input('Enter amount: '))
+        self.system.add_deposit(donor_id, blood_type, expiry_date, amount)
+        print('Deposit added successfully')
+
+    def remove_deposit(self):
+        deposit_id = int(input('Enter deposit ID: '))
+        self.system.remove_deposit(deposit_id)
+        print('Deposit removed successfully')
+
+    def filter_blood(self):
+        filter_attribute = input('Enter (Type | Amount) to specify filter attribute: ')
+        if re.match(r'^(Type|Amount)$', filter_attribute):
+            attribute = attribute_to_int(filter_attribute)
+            if (filter_attribute == 'Type'):
+                option = input('Enter (A|B|AB|O)[+-] to specify blood type: ')
+                if re.match(r'^(A|B|AB|O)[+-]$', option):
+                    value = type_to_int(option)
+                    filtered = self.system.filter_by_attribute(attribute,value)
+                    display_results(filtered)
+                else:
+                    print('Unrecognised blood type, please try again.')
+                    self.filter_blood()
+            elif (filter_attribute == 'Amount'):
+                option = input('Enter amount to filter deposits by: ')
+                if re.match(r'^[0-9]+$', option):
+                    value = int(option)
+                    filtered = self.system.filter_by_attribute(attribute,value)
+                    display_results(filtered)
+                else:
+                    print('Unrecognised amount, please enter a number and try again.')
+                    self.filter_blood()
+        else:
+            print('Unrecognised blood type, please try again.')
+            self.filter_blood()
+
+    def print_instructions(self):
+        print(
+'''
+----------------------------------------------------------------
+This system supports the following commands for Administrators:
+----------------------------------------------------------------
+| count          | count number of blood deposits              |
+----------------------------------------------------------------
+| volume         | count total volume of blood deposits        |
+----------------------------------------------------------------
+| add deposit    | add blood deposit to system                 |
+----------------------------------------------------------------
+| remove deposit | remove blood deposit from system            |
+----------------------------------------------------------------
+| add donor      | add donor record to the system              |
+----------------------------------------------------------------
+| remove donor   | remove donor record from the system         |
+----------------------------------------------------------------
+| help           | print all available commands                |
+----------------------------------------------------------------
+'''
+        )
+    
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from model.administrator import Administrator
 from model.hospital import Hospital
 from model.donor import Donor
 from misc.utility_functions import *
+from misc.messages import *
 import re
 
 def login(current_user):
@@ -12,168 +13,60 @@ def login(current_user):
     elif current_user == 'H':
         return Hospital(system)
     elif current_user == 'D':
-        return Donor(system)
+        first_name = input('Enter first name: ')
+        last_name = input('Enter last name: ')
+        age = int(input('Enter age: '))
+        blood_type = input('Enter blood type (A|B|AB|O)[+-]: ')
+        email = input('Enter email: ')
+        allergens = input('Enter allergens (if applicable): ')
+        return Donor(system, first_name, last_name, age, blood_type, email, allergens)
     else:
         print('Invalid user. Please try again')
         current_user = input('Please enter your user type (A/H/D): ')
         user = login(current_user)
         return user
 
-def print_instructions():
-    print('This system accepts the following commands:')
-    print('count - count number of blood deposits')
-    print('volume - count total volume of blood deposits')
-    print('add deposit - add blood deposit to system')
-    print('remove deposit - remove blood deposit from system')
-    print('request blood - request blood deposit from system')
-    print('add donor - add donor record to the system')
-    print('remove donor - remove donor record from system')
-    print('help - print all available commands')
+if __name__ == '__main__':
 
-def handle_count():
-    option = input('Enter (A|B|AB|O)[+-] to specify type [optional]: ')
-    if not option:
-        print(system.count_deposits())
-    elif re.match(r'^(A|B|AB|O)[+-]$', option):
-        blood_type = type_to_int(option)
-        print(system.count_deposits(blood_type))
-    else:
-        print('Unrecognised blood type, please try again.')
-        handle_count()
+    print(WELCOME_MESSAGE)
+    system = System()
+    current_user = input('Please enter your user type (A/H/D): ')
+    user = login(current_user)
+    print('Logged in as ' + user.get_type())
+    user.print_instructions()
+    user.warn_scarce_blood_types()
 
-def handle_volume():
-    option = input('Enter (A|B|AB|O)[+-] to specify type [optional]: ')
-    if not option:
-        print(system.count_volume())
-    elif re.match(r'^(A|B|AB|O)[+-]$', option):
-        blood_type = type_to_int(option)
-        print(system.count_volume(blood_type))
-    else:
-        print('Unrecognised blood type, please try again.')
-        handle_volume()
+    while True:
 
-def handle_add_deposit():
-    donor_id = int(input('Enter donor ID: '))
-    blood_type = type_to_int(input('Enter blood type (A|B|AB|O)[+-]: '))
-    expiry_date = date_to_int(input('Enter expiry date (dd/mm/yyyy): '))
-    amount = int(input('Enter amount: '))
-    user.add_deposit(donor_id, blood_type, expiry_date, amount)
-    print('Deposit added successfully')
+        action = input('Enter a command: ')
 
-def handle_remove_deposit():
-    deposit_id = int(input('Enter deposit ID: '))
-    user.remove_deposit(deposit_id)
-    print('Deposit removed successfully')
+        if action == 'help':
+            user.print_instructions()
+        
+        elif action == 'count':
+            user.count_deposits()
+        
+        elif action == 'volume':
+            user.count_volume()
 
-def handle_filter():
-    filter_attribute = input('Enter (Type | Amount) to specify filter attribute: ')
-    if re.match(r'^(Type|Amount)$', filter_attribute):
-        attribute = attribute_to_int(filter_attribute)
-        if (filter_attribute == 'Type'):
-            option = input('Enter (A|B|AB|O)[+-] to specify blood type: ')
-            if re.match(r'^(A|B|AB|O)[+-]$', option):
-                value = type_to_int(option)
-                filtered = system.filter_by_attribute(attribute,value)
-                display_results(filtered)
-            else:
-                print('Unrecognised blood type, please try again.')
-                handle_filter()
-        elif (filter_attribute == 'Amount'):
-            option = input('Enter amount to filter deposits by: ')
-            if re.match(r'^[0-9]+$', option):
-                value = int(option)
-                filtered = system.filter_by_attribute(attribute,value)
-                display_results(filtered)
-            else:
-                print('Unrecognised amount, please enter a number and try again.')
-                handle_filter()
-    else:
-        print('Unrecognised blood type, please try again.')
-        handle_filter()
-    
-def handle_request():
-    blood_type = type_to_int(input('Enter blood type (A|B|AB|O)[+-]: '))
-    amount = int(input('Enter amount: '))
-    if user.request_blood(blood_type, amount):
-        print('Request accepted.')
-    else:
-        print('Request rejected.')
+        elif action == 'add deposit':
+            user.add_deposit()
 
-def handle_add_donor():
-    if(user.get_user_type() != 'Administrator'):
-        print ("Only Administrators can add donors")
-        return
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
-    age = input("Enter age: ")
-    blood_type = type_to_int(input("Enter blood type (A|B|AB|O)[+-]: "))
-    email = input("Enter email: ")
-    allergens = input("Enter allergens: ")
-    user.add_donor(first_name, last_name, age, blood_type, email, allergens)
-    print("Donor added succesfully")
+        elif action == 'remove deposit':
+            user.remove_deposit()
+        
+        elif action == 'filter':
+            user.filter_blood()
 
-def handle_remove_donor():
-    if(user.get_user_type() != 'Administrator'):
-        print ("Only Administrators can add donors")
-        return
-    donor_id = int(input('Enter donor ID: '))
-    user.remove_donor(donor_id)
-    print('Donor removed successfully')
+        elif action == 'add donor':
+            user.add_donor()
+        
+        elif action == 'remove donor':
+            user.remove_donor()
 
-def warn_scarce_blood_types():
-    blood_total_lower_limit = 500
-    scarce = system.get_scarce_blood_types(blood_total_lower_limit)
-    if (scarce != []):
-        print_scarce_blood(scarce)
+        elif action == 'request blood':
+            user.request_blood()
 
-print('Welcome to the DafnyDuk Blood Managment System\n')
-
-print('Initialising system...\n')
-system = System()
-print('System initialised successfully.\n')
-
-print('This system accepts three different types of users:')
-print('A - Administrator')
-print('H - Hospital')
-print('D - Donor\n\n')
-current_user = input('Please enter your user type (A/H/D): ')
-user = login(current_user)
-
-print('Logged in as '+ str(user.get_user_type()))
-
-print_instructions()
-warn_scarce_blood_types()
-while True:
-
-    action = input('Enter a command: ')
-
-    if action == 'help':
-        print_instructions()
-    
-    elif action == 'count':
-        handle_count()
-    
-    elif action == 'volume':
-        handle_volume()
-
-    elif action == 'add deposit':
-        handle_add_deposit()
-
-    elif action == 'remove deposit':
-        handle_remove_deposit()
-    
-    elif action == 'filter':
-        handle_filter()
-
-    elif action == 'add donor':
-        handle_add_donor()
-    
-    elif action == 'remove donor':
-        handle_remove_donor()
-
-    elif action == 'request blood':
-        handle_request()
-
-    else:
-        print('Command not recognised, please try again')
-        continue
+        else:
+            print('Command not recognised, please try again')
+            continue
